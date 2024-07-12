5068a1ac1aca4b3f223621ccfbfce0f03ed65262
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-07-12 20:02:16 +0500

    add services files

--- TarteebATM/Program.cs
+++ TarteebATM/Program.cs
@@ -1,8 +1,35 @@
 ﻿using System;
-internal class Program
+
+namespace TarteebATM
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Tarteeb ATM ga hush kelibsiz!!!!");
+            Console.WriteLine("Dasturdan foydalanish uchun o'zingizga parol yarating!!\n");
+
+            Console.Write("Yangi parolni kiriting: ");
+            string userInputGreatePassword = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("========Tarteeb ATM========");
+            Console.WriteLine("Dasturdan foydalanish uchun yaratilgan parolmi kiriting!!!!\n");
+            Console.Write("Parolni kiriting: ");
+            string userInputPassword = Console.ReadLine();
+
+            do 
+            {
+                Console.WriteLine("parol");
+
+
+
+            } while (userInputPassword == userInputGreatePassword);
+            {
+                Console.WriteLine("dcdcdscdscdsc");
+            }
+
+
+        }
     }
 }
